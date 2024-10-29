49c87ccdcdbbb8104c0c81a3f7ee68fdeb7c8f30
Venkata Sai Vanaparthi <31667767+VenkatasaiVanaparthi@users.noreply.github.com>
2024-10-29 14:16:14 -0400

    Create resumee.tsx

---
+++ src/data/resumee.tsx
@@ -0,0 +1,65 @@
+export const DATA = {
+  name: "Venkata Sai Vanaparthi",
+  initials: "SV",
+  url: "https://raksha-portfolio-delta.vercel.app/",
+  location: "Buffalo, NY",
+  locationLink: "https://www.google.com/maps/place/buffalo+ny",
+  description:
+    "Certified Cloud Engineer with a focus on building cloud-native solutions and a background in both DevOps and technical support. Passionate about solving complex issues and enhancing system reliability.",
+  summary:
+    "With over 4 years of experience, I have worked extensively on cloud infrastructure, technical solutions engineering, and cloud migrations. My journey includes working with tools like AWS, Databricks, and Fortinet to deliver scalable and secure solutions. I’m now targeting opportunities in IT Cloud Site Reliability to leverage my skills in a dynamic environment.",
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Java",
+    "Python",
+    "SQL",
+    "Docker",
+    "Kubernetes",
+    "Postgres",
+    "AWS",
+    "GCP",
+    "Ansible",
+    "Jenkins",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/projects", icon: ProjectIcon, label: "Projects" },
+  ],
+  contact: {
+    email: "vvanapar@buffalo.edu",
+    tel: "+12",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/VenkatasaiVanaparthi",
+        icon: Icons.github,
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/vvanaparthi",
+        icon: Icons.linkedin,
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://dub.sh/venkata-x",
+        icon: Icons.x,
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://dub.sh/venkata-youtube",
+        icon: Icons.youtube,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+        navbar: false,
+      },
+    },
+  },
+};
